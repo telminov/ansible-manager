c83f6541839cbc28f72a5ca22208b4d77800ade1
sergey@telminov.ru <sergey@telminov.ru>
2017-03-23 14:02:29 +0300

    fix #9 - переработал параллельную обработку вывода комманды

--- core/datatools/tasks.py
+++ core/datatools/tasks.py
@@ -2,9 +2,9 @@ import os
 import signal
 import traceback
 from time import sleep
-
 from multiprocessing import Process
-from subprocess import PIPE, Popen
+import asyncio
+from asyncio.subprocess import PIPE, create_subprocess_shell
 
 from core import consts
 from core import models
@@ -14,17 +14,16 @@ class TaskManager:
     def run(self):
         """ Tasks monitoring and check status"""
         while True:
-            self.check_in_progress()
-            self.start_wait_tasks()
+            self.check_in_progress_tasks()
+            self.start_waiting_tasks()
             sleep(1)
 
-    def check_in_progress(self):
+    def check_in_progress_tasks(self):
         tasks = models.Task.objects.filter(status=consts.IN_PROGRESS, pid__isnull=False)
         not_run_tasks = []
         for task in tasks:
             if not task.pid:
-                models.TaskLog.objects.create(
-                    task=task,
+                task.logs.create(
                     status=consts.FAIL,
                     message='Error "pid is None"',
                 )
@@ -37,73 +36,62 @@ class TaskManager:
             task.status = consts.FAIL
             task.save()
 
-            models.TaskLog.objects.create(
-                task=task,
+            task.logs.create(
                 status=consts.FAIL,
                 message='Task with pid %s is not running' % task.pid
             )
 
-    def start_wait_tasks(self):
+    def start_waiting_tasks(self):
         tasks = models.Task.objects.filter(status=consts.WAIT)
         for task in tasks:
             self.run_task_process(task)
 
-    @staticmethod
-    def run_task(task_id):
+    @classmethod
+    def run_task(cls, task_id):
         from django.db import connection
         connection.connection.close()
         connection.connection = None
 
         task = models.Task.objects.get(id=task_id)
 
-        task.logs.create(
-            status=consts.IN_PROGRESS,
-            message='Run with pid %s in new process' % task.pid,
-        )
-
         command = task.get_command(splited=True)
+        shell_command = ' '.join(command)
         inventory_file_path = command[2]
         task.logs.create(
             status=consts.IN_PROGRESS,
-            message='Command: %s' % ' '.join(command),
+            message='Command: %s' % shell_command,
         )
 
         try:
-            proc = Popen(command, stdout=PIPE, stderr=PIPE)
-            while proc.poll() is None:
-                output = proc.stdout.readline()
+            loop = asyncio.get_event_loop()
 
-                if output:
-                    models.TaskLog.objects.create(
-                        task=task,
-                        status=consts.IN_PROGRESS,
-                        output=output
-                    )
+            create_proc = create_subprocess_shell(shell_command, stdout=PIPE, stderr=PIPE)
+            proc = loop.run_until_complete(create_proc)
+
+            tasks = [
+                proc.wait(),
+                cls._log_output(task, proc.stdout, consts.IN_PROGRESS),
+                cls._log_output(task, proc.stderr, consts.FAIL),
+            ]
+            loop.run_until_complete(asyncio.wait(tasks))
 
             code = proc.returncode
             if code == 0:
-                output = proc.stdout.readline()
-                if output:
-                    models.TaskLog.objects.create(
-                        task=task,
-                        status=consts.IN_PROGRESS,
-                        output=output
-                    )
+                task.logs.create(
+                    status=consts.COMPLETED,
+                    message='Completed.'
+                )
                 task.status = consts.COMPLETED
                 task.save()
-                models.TaskLog.objects.create(
-                    task=task,
+                task.logs.create(
                     status=consts.COMPLETED,
                     message='Task complete'
                 )
             else:
                 task.status = consts.FAIL
                 task.save()
-                _, err = proc.communicate()
-                models.TaskLog.objects.create(
-                    task=task,
+                task.logs.create(
                     status=consts.FAIL,
-                    output=err,
                     message='Failed with status code %s' % code
                 )
 
@@ -111,8 +99,7 @@ class TaskManager:
             task.status = consts.FAIL
             task.save()
             traceback_message = traceback.format_exc()
-            models.TaskLog.objects.create(
-                task=task,
+            task.logs.create(
                 output=traceback_message,
                 message='Progress error "%s"' % e,
                 status=consts.FAIL
@@ -131,8 +118,7 @@ class TaskManager:
                 os.kill(task.pid, signal.SIGTERM)
             except Exception as e:
                 traceback_message = traceback.format_exc()
-                models.TaskLog.objects.create(
-                    task=task,
+                task.logs.create(
                     output=traceback_message,
                     messgae='Stop error "%s"' % e,
                     status=consts.FAIL
@@ -140,8 +126,7 @@ class TaskManager:
 
         task.status = consts.STOPPED
         task.save()
-        models.TaskLog.objects.create(
-            task=task,
+        task.logs.create(
             status=consts.STOPPED,
             message='Task stopped'
         )
@@ -155,17 +140,27 @@ class TaskManager:
         connection.connection.close()
         connection.connection = None
 
-        models.TaskLog.objects.create(
-            task=task,
-            status=consts.IN_PROGRESS,
-            message='Start task with pid %s' % pid,
-        )
-
         task.pid = pid
         task.status = consts.IN_PROGRESS
         task.save()
+
+        task.logs.create(
+            status=consts.IN_PROGRESS,
+            message='Start task with pid %s' % pid,
+        )
 
     @staticmethod
     def _is_process_run(pid):
         return os.path.exists("/proc/%s" % pid)
 
+    @staticmethod
+    async def _log_output(task, stream, status):
+        while True:
+            output = await stream.readline()
+            if not output:
+                return
+
+            task.logs.create(
+                status=status,
+                output=output,
+            )
