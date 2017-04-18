69731f6932da5d968a039f4213d931a4b02f9914
Arseny Kirichenko <mr.ars156@gmail.com>
2017-04-18 15:26:33 +0300

    #20 Решение от YraganTron(после ревью)

--- core/forms/host_group.py
+++ core/forms/host_group.py
@@ -9,10 +9,8 @@ class Search(forms.Form):
 
 class Edit(forms.ModelForm):
     name = forms.CharField()
-
     hosts = forms.ModelMultipleChoiceField(widget=forms.SelectMultiple(attrs={'class': 'need-select2'}),
                                            queryset=models.Host.objects.all(), required=False)
-
 
     class Meta:
         model = models.HostGroup
