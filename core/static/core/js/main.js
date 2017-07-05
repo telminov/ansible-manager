6f91a895f57cbf3049dc79d10030330e8e240185
Arseny Kirichenko <mr.ars156@gmail.com>
2017-07-05 20:20:58 +0300

    #67 правка шаблонов (после ревью)

--- core/static/core/js/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by YraganTron on 05.07.17.
- */
-
-$(document).ready(function () {
-    $('#repeat-setting').on('click', function (e) {
-        window.location.href += 'repeat/'
-    })
-});
